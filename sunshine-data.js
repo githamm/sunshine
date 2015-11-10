806cb13fde7b5737149a467a188a71443676650c
githamm <hamm.kevin@gmail.com>
2015-11-10 14:07:50 -0700

    Add footable

---
+++ sunshine-data.js
@@ -0,0 +1,452 @@
+[
+  {
+    "state": "Alabama",
+    "publicInfo": "33 (40.6)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "42 (41.5)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "27 (66)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "Alaska",
+    "publicInfo": "34 (37.5)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "2 (88.2)",
+    "politicalFinancingGrade": "grade-b",
+    "electoralOversight": "25 (69.4)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "Arizona",
+    "publicInfo": "20 (48.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "25 (61)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "13 (73.6)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Arkansas",
+    "publicInfo": "16 (52.2)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "30 (55.3)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "29 (62.5)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "California",
+    "publicInfo": "28 (43.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "10 (71)",
+    "politicalFinancingGrade": "grade-c",
+    "electoralOversight": "17 (71.5)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Colorado",
+    "publicInfo": "34 (37.5)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "26 (60.8)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "8 (76.4)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Connecticut",
+    "publicInfo": "7 (58.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "8 (72.4)",
+    "politicalFinancingGrade": "grade-c",
+    "electoralOversight": "16 (72.2)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Delaware",
+    "publicInfo": "24 (45.6)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "27 (59)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "23 (69.4)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "Florida",
+    "publicInfo": "17 (50.6)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "28 (58.7)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "46 (50)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Georgia",
+    "publicInfo": "28 (43.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "38 (45.3)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "14 (72.9)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Hawaii",
+    "publicInfo": "2 (70)",
+    "publicInfoGrade": "grade-c",
+    "politicalFinancing": "11 (70.6)",
+    "politicalFinancingGrade": "grade-c",
+    "electoralOversight": "34 (59.7)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Idaho",
+    "publicInfo": "6 (61.3)",
+    "publicInfoGrade": "grade-d",
+    "politicalFinancing": "15 (67.4)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "3 (84.7)",
+    "electoralOversightGrade": "grade-b"
+  },
+  {
+    "state": "Illinois",
+    "publicInfo": "15 (52.5)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "7 (74.1)",
+    "politicalFinancingGrade": "grade-c",
+    "electoralOversight": "15 (72.2)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Indiana",
+    "publicInfo": "23 (46.9)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "47 (37.7)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "25 (69.4)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "Iowa",
+    "publicInfo": "1 (72.8)",
+    "publicInfoGrade": "grade-c",
+    "politicalFinancing": "24 (61.6)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "31 (61.1)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "Kansas",
+    "publicInfo": "13 (53.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "6 (74.9)",
+    "politicalFinancingGrade": "grade-c",
+    "electoralOversight": "37 (57.6)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Kentucky",
+    "publicInfo": "8 (58.1)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "4 (81.5)",
+    "politicalFinancingGrade": "grade-b",
+    "electoralOversight": "28 (64.6)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "Louisiana",
+    "publicInfo": "46 (29.7)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "21 (64)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "23 (69.4)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "Maine",
+    "publicInfo": "26 (45.3)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "19 (65.5)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "47 (45.1)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Maryland",
+    "publicInfo": "41 (34.1)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "22 (62.7)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "9 (75)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Massachusetts",
+    "publicInfo": "40 (34.4)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "1 (92)",
+    "politicalFinancingGrade": "grade-a",
+    "electoralOversight": "6 (80.6)",
+    "electoralOversightGrade": "grade-b"
+  },
+  {
+    "state": "Michigan",
+    "publicInfo": "42 (33.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "32 (51.6)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "5 (81.3)",
+    "electoralOversightGrade": "grade-b"
+  },
+  {
+    "state": "Minnesota",
+    "publicInfo": "24 (45.6)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "17 (66.8)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "6 (80.6)",
+    "electoralOversightGrade": "grade-b"
+  },
+  {
+    "state": "Mississippi",
+    "publicInfo": "21 (47.2)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "50 (33.8)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "42 (56.3)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Missouri",
+    "publicInfo": "18 (50.3)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "44 (40.6)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "42 (56.3)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Montana",
+    "publicInfo": "42 (33.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "15 (67.4)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "17 (71.5)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Nebraska",
+    "publicInfo": "8 (58.1)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "39 (44.5)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "2 (85.4)",
+    "electoralOversightGrade": "grade-b"
+  },
+  {
+    "state": "Nevada",
+    "publicInfo": "28 (43.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "34 (49.9)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "35 (58.3)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "New Hampshire",
+    "publicInfo": "49 (24.4)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "33 (51.3)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "36 (58.3)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "New Jersey",
+    "publicInfo": "27 (44.1)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "14 (67.7)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "50 (22.2)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "New Mexico",
+    "publicInfo": "19 (49.1)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "36 (47.9)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "33 (60.4)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "New York",
+    "publicInfo": "11 (55.3)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "23 (62.4)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "49 (38.9)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "North Carolina",
+    "publicInfo": "39 (35.6)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "5 (75.3)",
+    "politicalFinancingGrade": "grade-c",
+    "electoralOversight": "20 (70.8)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "North Dakota",
+    "publicInfo": "10 (56.6)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "45 (39.5)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "1 (86.8)",
+    "electoralOversightGrade": "grade-b"
+  },
+  {
+    "state": "Ohio",
+    "publicInfo": "47 (29.1)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "18 (66.6)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "9 (75)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Oklahoma",
+    "publicInfo": "44 (32.5)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "13 (68.6)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "37 (57.6)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Oregon",
+    "publicInfo": "34 (37.5)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "49 (36.5)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "11 (74.3)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Pennsylvania",
+    "publicInfo": "4 (67.8)",
+    "publicInfoGrade": "grade-d",
+    "politicalFinancing": "43 (41.1)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "48 (41)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Rhode Island",
+    "publicInfo": "5 (64.7)",
+    "publicInfoGrade": "grade-d",
+    "politicalFinancing": "9 (71.6)",
+    "politicalFinancingGrade": "grade-c",
+    "electoralOversight": "37 (57.6)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "South Carolina",
+    "publicInfo": "44 (32.5)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "37 (47.5)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "30 (61.8)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "South Dakota",
+    "publicInfo": "21 (47.2)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "35 (48.8)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "42 (56.3)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Tennessee",
+    "publicInfo": "37 (37.2)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "20 (65.1)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "12 (73.6)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Texas",
+    "publicInfo": "48 (25.9)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "41 (43)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "45 (52.1)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Utah",
+    "publicInfo": "2 (70)",
+    "publicInfoGrade": "grade-c",
+    "politicalFinancing": "40 (43.7)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "17 (71.5)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Vermont",
+    "publicInfo": "12 (55)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "3 (84.3)",
+    "politicalFinancingGrade": "grade-b",
+    "electoralOversight": "31 (61.1)",
+    "electoralOversightGrade": "grade-d"
+  },
+  {
+    "state": "Virginia",
+    "publicInfo": "38 (35.9)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "46 (39.3)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "4 (81.9)",
+    "electoralOversightGrade": "grade-b"
+  },
+  {
+    "state": "Washington",
+    "publicInfo": "32 (42.2)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "12 (69.9)",
+    "politicalFinancingGrade": "grade-d",
+    "electoralOversight": "20 (70.8)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "West Virginia",
+    "publicInfo": "28 (43.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "31 (55.2)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "40 (56.9)",
+    "electoralOversightGrade": "grade-f"
+  },
+  {
+    "state": "Wisconsin",
+    "publicInfo": "13 (53.8)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "29 (57.5)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "20 (70.8)",
+    "electoralOversightGrade": "grade-c"
+  },
+  {
+    "state": "Wyoming",
+    "publicInfo": "50 (22.5)",
+    "publicInfoGrade": "grade-f",
+    "politicalFinancing": "48 (36.9)",
+    "politicalFinancingGrade": "grade-f",
+    "electoralOversight": "40 (56.9)",
+    "electoralOversightGrade": "grade-f"
+  }
+]
